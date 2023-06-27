0a922f2187bcb42008c8e3a3e76572d522e06caa
Lei <45155667+leifu1128@users.noreply.github.com>
2023-06-27 20:36:18 +0800

    Update download.py

--- scripts/download.py
+++ scripts/download.py
@@ -3,5 +3,6 @@ from src import MPApply, get_trading_days
 
 
 if __name__ == '__main__':
-    src.midas.DATA_DIR = 'spx_opra_raw'
-    MPApply(['SPX'], get_trading_days(20190102, 20211231)).apply(src.midas.download, 1)
+    src.midas.DATA_DIR = 'scratch/su_spy_2023_2'
+    src.midas.SYMBOL_DIR = 'symbols/for_su/spy_{}.csv'
+    MPApply(['SPY'], get_trading_days('20230103', '20230112')).apply(src.midas.download, 4)
