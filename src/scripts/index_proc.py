1dea2ee1e1e89baed59f644e95a24cec411b0c84
Lei <45155667+leifu1128@users.noreply.github.com>
2023-08-10 10:13:13 -0400

    Update index_proc.py

--- src/scripts/index_proc.py
+++ src/scripts/index_proc.py
@@ -4,9 +4,11 @@ from src import MPApply, get_trading_days
 
 
 if __name__ == '__main__':
-    src.midas.DATA_DIR = 'scratch/spy23_25-27'
-    src.midas.OUT_DIR = 'scratch/spy23_25-27_out'    
+    src.midas.DATA_DIR = 'scratch/spy_jan_17'
+    src.midas.OUT_DIR = 'scratch/spy_jan_out'    
     
-    args = [('SPY', '20230127', filename) for filename in os.listdir(f'{src.midas.DATA_DIR}/20230127/SPY')]
+    args = []
+    for date in get_trading_days('20230117', '20230120'):
+        args += [('SPY', date, filename) for filename in os.listdir(f'{src.midas.DATA_DIR}/{date}/SPY')]
     
-    MPApply(args).apply(src.midas.index_worker, 4)
+    MPApply(args).apply(src.midas.index_worker, 8)
