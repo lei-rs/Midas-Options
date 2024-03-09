ecb66c21a98856ba9f04b6c9c6f9fd55894d06cb
Lei <45155667+lei-rs@users.noreply.github.com>
2024-03-09 18:28:06 -0500

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,9 +1,9 @@
-import cmd
 import os
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Manager
 from pathlib import Path
 
+import argparse
 import pandas
 import polars as pl
 from tqdm import tqdm
@@ -22,7 +22,7 @@ def save_result(df: pandas.DataFrame | pl.DataFrame, path_in: str, path_out: str
     path_out = Path(path_out) / f"{date}_{under}.csv"
 
     if not os.path.exists(path_out):
-        f = open(path_out, "w")
+        f = open(path_out, "x")
         f.write(",".join(df.columns) + "\n")
         f.close()
 
@@ -49,51 +49,60 @@ def par_generate(fn):
     return par_fn
 
 
-class Midas(cmd.Cmd):
-    def do_generate(self, args):
-        try:
-            args = args.split()
-            if len(args) == 3:
-                in_dir, out_dir, kind = args
-                workers = None
-            else:
-                in_dir, out_dir, kind, workers = args
-                workers = int(workers)
-        except ValueError:
-            raise ValueError("Invalid arguments")
+def do_generate(in_dir: str, out_dir: str, kind: str, workers: int = None):
+    if not os.path.exists(in_dir):
+        raise FileNotFoundError(f"Directory {in_dir} does not exist")
+    os.makedirs(out_dir)
+    if kind not in PROC_FN:
+        raise ValueError(f"Invalid report type to generate: {kind}")
 
-        if not os.path.exists(in_dir):
-            raise FileNotFoundError(f"Directory {in_dir} does not exist")
-        if os.path.exists(out_dir):
-            raise FileExistsError(f"Directory {out_dir} already exists")
-        if kind not in PROC_FN:
-            raise ValueError(f"Invalid report type to generate: {kind}")
+    raw_files = list(Path(in_dir).rglob("*.parquet.gzip"))
+    if not len(raw_files):
+        raise FileNotFoundError(f"No files to process in {in_dir}")
 
-        raw_files = list(Path(in_dir).rglob("*.parquet.gzip"))
-        if not len(raw_files):
-            raise FileNotFoundError(f"No files to process in {in_dir}")
-
-        fn = PROC_FN[kind]
-        if workers and workers > 1:
-            pool = ProcessPoolExecutor()
-            m = Manager()
-            lock = m.Lock()
-            for result in tqdm(
-                pool.map(
-                    par_generate(fn),
-                    [lock] * len(raw_files),
-                    [str(file) for file in raw_files],
-                    [out_dir] * len(raw_files),
-                ),
-                total=len(raw_files),
-            ):
-                if result:
-                    raise result
-        else:
-            for file in tqdm(raw_files):
-                df = fn(str(file))
-                save_result(df, str(file), out_dir)
+    fn = PROC_FN[kind]
+    if workers and workers > 1:
+        pool = ProcessPoolExecutor()
+        m = Manager()
+        lock = m.Lock()
+        for result in tqdm(
+            pool.map(
+                par_generate(fn),
+                [lock] * len(raw_files),
+                [str(file) for file in raw_files],
+                [out_dir] * len(raw_files),
+            ),
+            total=len(raw_files),
+        ):
+            if result:
+                raise result
+    else:
+        for file in tqdm(raw_files):
+            df = fn(str(file))
+            save_result(df, str(file), out_dir)
 
 
 if __name__ == "__main__":
-    Midas().cmdloop()
+    parser = argparse.ArgumentParser(prog="Midas Options CLI")
+    parser.add_argument(
+        "in_dir",
+        type=str,
+        help="Directory containing the raw OPRA data",
+    )
+    parser.add_argument(
+        "out_dir",
+        type=str,
+        help="Directory to save the generated reports",
+    )
+    parser.add_argument(
+        "kind",
+        type=str,
+        help="Type of report to generate",
+    )
+    parser.add_argument(
+        "--workers",
+        type=int,
+        help="Number of workers to use for multiprocessing",
+    )
+    args = parser.parse_args()
+    do_generate(args.in_dir, args.out_dir, args.kind, args.workers)
