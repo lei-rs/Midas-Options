b34336738a2e15d8d57b45ddd0da85ef09fa8469
Lei <45155667+lei-rs@users.noreply.github.com>
2024-03-29 21:42:47 -0400

    Update count.py

--- src/count.py
+++ src/count.py
@@ -5,34 +5,20 @@ from polars import DataFrame
 def count_cc(path: str) -> DataFrame:
     df = pl.read_parquet(path)
     symbol = df.item(0, "c6")
-    temp = pl.DataFrame(
-        {
-            "c15": ["A", "B", "C", "O"],
-            "count": [0, 0, 0, 0],
-        }
-    ).lazy()
     df = (
-        df.lazy()
+        df
+        .select("c1", "c15")
         .filter(pl.col("c1") == "F@")
-        .select("c15")
         .groupby("c15")
         .agg(pl.count())
-        .filter(pl.col("c15").is_in(["A", "B", "C", "O"]))
+        .filter(pl.col("c15").is_in({"A", "B", "C", "O", "F"}))
+        .transpose(column_names="c15")
     )
-    df = (
-        df.join(temp, on="c15", how="outer")
-        .fill_null(0)
-        .sort("c15")
-        .select(pl.col("count") + pl.col("count_right"))
-        .collect()
-    )
-    df = (
-        df.transpose(column_names=["A", "B", "C", "O"])
-        .with_columns(
-            [
-                pl.Series("symbol", [symbol]),
-            ]
-        )
-        .select(["date", "symbol", "A", "B", "C", "O"])
-    )
-    return df
+    cols = df.columns
+    new_cols = []
+    for c in ["A", "B", "C", "O", "F"]:
+        if c not in cols:
+            new_cols.append(pl.lit(0).alias(c))
+    new_cols.append(pl.lit(symbol).alias("symbol"))
+    df = df.with_columns(new_cols)
+    return df.select(["symbol", "A", "B", "C", "O", "F"])
