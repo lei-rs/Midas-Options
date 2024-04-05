4b36b7ed406e5780ea118297608078ad84ffcd9c
Lei <45155667+lei-rs@users.noreply.github.com>
2024-04-05 00:47:32 -0400

    delete scripts

--- src/scripts/download.py
+++
@@ -1,9 +0,0 @@
-import src.midas
-from src import MPApply, get_trading_days
-
-
-if __name__ == '__main__':
-    src.midas.DATA_DIR = 'scratch/su_spy_2023_2'
-    src.midas.SYMBOL_DIR = 'symbols/for_su/spy_{}.csv'
-    args = [('SPY', date) for date in get_trading_days('20230103', '20230112')]
-    MPApply(args).apply(src.midas.download, 4)
